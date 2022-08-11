22760045a465ea255c9aee51b0a116d7dedf5abf
michael_rosario <michael.rosario@designinteractive.net>
2022-08-10 23:54:03 -0400

    Implement get weekend ending date

--- src/app/edit-time-sheet/edit-time-sheet.component.ts
+++ src/app/edit-time-sheet/edit-time-sheet.component.ts
@@ -20,15 +20,41 @@ export class EditTimeSheetComponent implements OnInit {
 
   ngOnInit(): void {
     this.makeNewTimeSheet();
+  }
 
+  // 2022-08-12
+  getWeekEndingDate(){
+    // get current date
+    let currentDate = new Date();
+    let day = currentDate.getDay();
+    
+    // if current date is less than friday
+    if(day < 5){
+      let k = 5 - day - 1;
+      const date = currentDate;
+      date.setDate(date.getDate() + k);      
+      currentDate = date;
+    }
+    else if(day > 5)
+    {
+      let k = day - 5 - 1;
+      const date = currentDate;
+      date.setDate(date.getDate() - k);      
+      currentDate = date;
+    }
+
+    let response = currentDate.toISOString().split('T')[0];
+    return response;
   }
 
   private makeNewTimeSheet() {
+    this.getWeekEndingDate();
     this.timeSheet = new TimeSheet();
     this.timeSheet.createdAt = Date.now();
     this.timeSheet.createdBy = this.getUserId();
     this.timeSheet.id = uuidv4();
     this.timeSheet.notes = "";
+    this.timeSheet.weekEnding = this.getWeekEndingDate();
   }
 
   onAddEntry()
